192020bf20fb75501e83700fc3fbbe456b658fe2
Anand Chitipothu <anandology@gmail.com>
2022-08-10 13:41:31 +0530

    Inject assets into the generated html.
    - Added a placeholder feather.js file in assets and that gets included the generated html
    
    Issue #1

--- mkdocs_feather/__init__.py
+++ mkdocs_feather/__init__.py
@@ -1,5 +1,38 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import get_files
+import os
 
 class FeatherPlugin(BasePlugin):
     def on_config(self, config, **kwargs):
+        self.add_markdown_extension(config, "fenced_code")
+        print("on_config", config)
         return config
+
+    def on_files(self, files, config):
+        self.inject_assets(files, config)
+
+    def inject_assets(self, files, config):
+        root = os.path.join(os.path.dirname(__file__), "assets")
+        print("inject_assets", root)
+        docs_dir = config["docs_dir"]
+        config2 = dict(config, docs_dir=root)
+        assets = get_files(config2)
+
+        css = []
+        js = []
+        for file in assets:
+            path = file.src_path.replace("\\", "/")
+            if path.endswith(".css"):
+                files.append(file)
+                css.append(path)
+            elif path.endswith(".js"):
+                files.append(file)
+                js.append(path)
+
+        config["extra_css"] = css
+        config["extra_javascript"] = js
+
+    def add_markdown_extension(self, config, name):
+        exts = config['markdown_extensions']
+        if  name not in exts:
+            exts.append(name)
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="mkdocs-feather",
+    version="0.1.0",
     description="Mkdocs plugin to enable live code examples in the documentation using feather",
     author="Anand Chitipothu",
     author_email="anand@pipal.in",
     install_requires=["mkdocs"],
     url="https://github.com/pipalacademy/mkdocs-feather",
-
     packages=find_packages(),
-
+    include_package_data=True,
     entry_points={
         'mkdocs.plugins': [
             'feather = mkdocs_feather:FeatherPlugin',
